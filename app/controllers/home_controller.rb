6fe1a6d0e77c09dab96fde7bd57058ae76d91953
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-14 17:00:21 -0600

    initial setup

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+  def index
+    # GovTrackService.get_reps
+    # @legislators = Legislators.all
+  end
+end
---
+++ app/services/gov_track_service.rb
@@ -0,0 +1,13 @@
+class GovTrackService
+
+  def self.get_reps
+    response = parse(Faraday.get("https://www.govtrack.us/api/v2/role?current=true&limit=600"))
+    Legislators.find_or_create_by(response["objects"])
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_name: true)
+  end
+end
